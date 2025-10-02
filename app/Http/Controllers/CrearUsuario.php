ca1834be148734417172f8ef76339ab8094b7d5e
dbitz <dbielosp@gmail.com>
2025-10-02 17:12:30 -0500

    modificar controlador crearusuario

--- app/Http/Controllers/CrearUsuario.php
+++ app/Http/Controllers/CrearUsuario.php
@@ -9,11 +9,13 @@ use App\Models\User;
 
 class CrearUsuario extends Controller
 {
-    public function showRegistrationForm(){
-        return view('auth.blade');
+    public function showRegistrationForm()
+    {
+        return view('registro.registro');
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
@@ -22,6 +24,12 @@ class CrearUsuario extends Controller
 
         $user = User::create([
             'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
     }
 }
