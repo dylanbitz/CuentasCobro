e2e306cec8ca769aacfb8017fd0acaa815bd7824
dbitz <dbielosp@gmail.com>
2025-10-16 16:52:27 -0500

    modificar migrations de usuario y crear migration de roles
    en usuarios se cambiaron varios nombres de inglés a español

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->timestamp('email_verificado_en')->nullable();
+            $table->string('contrasenia');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -24,7 +24,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->timestamp('creado_en')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
